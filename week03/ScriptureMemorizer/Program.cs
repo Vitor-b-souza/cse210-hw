3c928aea116288a603ed3ad168dc8a8820a50102
Vitor-b-souza <vitor.souza001@gmail.com>
2025-11-13 08:52:23 -0300

    text fix

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -188,7 +188,7 @@ namespace ScriptureMemorizer
             var scriptures = new List<(Reference, string)>
             {
                 (new Reference("John", 3, 16), "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."),
-                (new Reference("Proverbs", 3, 5, 6), "¶ Trust in the Lord with all thine heart; and lean not unto thine own understanding; In all thy ways acknowledge him, and he shall direct thy paths."),
+                (new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all thine heart; and lean not unto thine own understanding; In all thy ways acknowledge him, and he shall direct thy paths."),
                 (new Reference("1 Nephi", 3, 7), "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.")
             };
 
